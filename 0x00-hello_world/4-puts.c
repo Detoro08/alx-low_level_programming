854e7ee88693f611f5e2078df022e82426f9e98b
Detoro08 <Princessdetoro08@gmail.com>
2022-09-08 14:01:21 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 
 /**
  *  * main - Entry point
- * 
+ *
  *  *
  *
  *  * Return: Always 0 (Success)
@@ -15,7 +15,7 @@ int main(void)
 
 {
 
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
